da0f0ba0bef20b257ffb493b4eacd546496258e5
Markus Hergenröder <markus@coding4glory.net>
2025-05-03 11:11:37 +0200

    Don't stress github

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
     { import = "plugins" },
   },
   rocks = {
-    enabled = false, 
+    enabled = false,
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
