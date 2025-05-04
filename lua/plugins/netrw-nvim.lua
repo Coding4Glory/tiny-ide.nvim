f8588810317e324f227b38dad54c62ad627ad2a4
Markus Hergenröder <markus@coding4glory.net>
2025-05-04 12:46:40 +0200

    changed icons to simple chars, changed inspection keymap

--- lua/plugins/netrw-nvim.lua
+++ lua/plugins/netrw-nvim.lua
@@ -1,12 +1,17 @@
 return {
-    "prichrd/netrw.nvim", 
-    opts = {
-        use_devicons = true,
-        mappings = {
-            ['<leader>ei'] = function(payload) print(vim.inspect(payload)) end,
-        },
-        user_mappings = {
-            { "<c-p>", 'lua print("hello world")' }
-        }
-    }
+    "prichrd/netrw.nvim",
+    config = function()
+        require("netrw").setup({
+            icons = {
+                symlink = '<',
+                file = '-',
+                directory = '+'
+            },
+            -- use_devicons = true,
+            mappings = {
+                ['K'] = function(payload) print(vim.inspect(payload)) end,
+            },
+        })
+        vim.keymap.set('n', '<leader>te', ":15Lex<CR>", { desc = "[t]oggle [e]xplorer" })
+    end,
 }
