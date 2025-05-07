b665ef7aab4907cdc58aebe6e1c55681403e225f
Markus Hergenröder <markus@coding4glory.net>
2025-05-07 19:54:26 +0200

    prepared function for planned auto open

--- lua/plugins/netrw-nvim.lua
+++ lua/plugins/netrw-nvim.lua
@@ -1,6 +1,7 @@
 return {
     "prichrd/netrw.nvim",
     config = function()
+        local open_tree = function() vim.cmd("15Lex") end
         require("netrw").setup({
             icons = {
                 symlink = '<',
@@ -12,6 +13,9 @@ return {
                 ['K'] = function(payload) print(vim.inspect(payload)) end,
             },
         })
-        vim.keymap.set('n', '<leader>te', ":15Lex<CR>", { desc = "[t]oggle [e]xplorer" })
+        vim.g.netrw_banner = 0
+        -- vim.g.netrw_browse_split = -3
+        vim.g.netrw_liststyle = 3
+        vim.keymap.set('n', '<leader>te', open_tree, { desc = "[t]oggle [e]xplorer" })
     end,
 }
