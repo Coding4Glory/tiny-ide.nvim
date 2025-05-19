1b2f49ddddc856dc123d271c9f3e5e2db2d3b285
Markus Hergenröder <markus@coding4glory.net>
2025-05-20 00:25:27 +0200

    fixed wrong description

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -9,9 +9,9 @@ return {
         vim.keymap.set('n', '<leader>tg', function() git.open({ kind = "split" }) end, { desc = 'toggle Neo[g]it' })
         vim.keymap.set('n', '<leader>gt', git.open, { desc = 'toggle Neo[g]it' })
         vim.keymap.set('n', '<leader>gc', function() git.open({ "commit", kind = "floating" }) end, { desc = 'git [c]ommit' })
-        vim.keymap.set('n', '<leader>gp', function() git.open({ "pull", kind = "floating" }) end, { desc = 'git [p]ull' }) 
-        vim.keymap.set('n', '<leader>gP', function() git.open({ "push" }) end, { desc = 'git [P]ush' }) 
-        vim.keymap.set('n', '<leader>gf', function() git.open({ "fetch", kind = "floating" }) end, { desc = 'git [f]etch' }) 
-        vim.keymap.set('n', '<leader>gr', function() git.open({ "rebase", kind = "floating" }) end, { desc = 'git [f]etch' }) 
+        vim.keymap.set('n', '<leader>gp', function() git.open({ "pull", kind = "floating" }) end, { desc = 'git [p]ull' })
+        vim.keymap.set('n', '<leader>gP', function() git.open({ "push" }) end, { desc = 'git [P]ush' })
+        vim.keymap.set('n', '<leader>gf', function() git.open({ "fetch", kind = "floating" }) end, { desc = 'git [f]etch' })
+        vim.keymap.set('n', '<leader>gr', function() git.open({ "rebase", kind = "floating" }) end, { desc = 'git [r]ebase' })
     end,
 }
