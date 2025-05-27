78c93e946c512238f3103f2fcd36fdd76d076aa9
Markus Hergenröder <markus@coding4glory.net>
2025-05-28 00:31:01 +0200

    compatible icons for nvim-tree and more lazy config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,21 +5,58 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-        require("nvim-tree").setup {
-            filters = {
-                custom = { "^\\.git$" }
+    keys = {
+        { "<leader>te", "<CMD>NvimTreeToggle<CR>", desc = "toggle file [e]xplorer" },
+    },
+    opts = {
+        filters = {
+            custom = { "^\\.git$" }
+        },
+        renderer = {
+            icons = {
+                glyphs = {
+                    default = ".",
+                    symlink = "🔗",
+                    bookmark = "🔖",
+                    modified = "*",
+                    hidden = "🤦🏽‍♀️",
+                    folder = {
+                        arrow_closed = "",
+                        arrow_open = "",
+                        default = "🗂",
+                        open = "📂",
+                        empty = "📁",
+                        empty_open = "📂",
+                        symlink = "🔗",
+                        symlink_open = "🖇"
+                    },
+                    git = {
+                        unstaged = "✗",
+                        staged = "✓",
+                        unmerged = "⚠",
+                        renamed = "➜",
+                        untracked = "★",
+                        deleted = "🗑",
+                        ignored = "🚫",
+                     }
+                }
             }
-        }
-        local api = require("nvim-tree.api")
-        vim.keymap.set('n', '<leader>te', api.tree.toggle, { desc = '[t]oggle file [e]xplorer'})
-        vim.api.nvim_create_autocmd("VimEnter", {
-            group = vim.api.nvim_create_augroup("always_open_tree", {}),
-            callback = function()
-                if not api.tree.is_visible() then
-                    api.tree.open()
-                end
-            end
-        })
-    end,
+        },
+    }
+    -- config = function()
+    --     require("nvim-tree").setup {
+    --         filters = {
+    --             custom = { "^\\.git$" }
+    --         }
+    --     }
+    --     local api = require("nvim-tree.api")
+    --     vim.keymap.set('n', '<leader>te', api.tree.toggle, { desc = '[t]oggle file [e]xplorer'})
+    --     vim.api.nvim_create_autocmd("VimEnter", {
+    --         group = vim.api.nvim_create_augroup("always_open_tree", {}),
+    --         callback = function()
+    --             if not api.tree.is_visible() then
+    --                 api.tree.open()
+    --             end
+    --         end
+    --     })
 }
