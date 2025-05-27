0efbb0b200ad39c26b10b634ac3d652b9264e720
Markus Hergenröder <markus@coding4glory.net>
2025-05-28 00:42:23 +0200

    synced with built in menu

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -23,6 +23,9 @@ return {
                     -- require("cmp.config").set_onetime({ sources = {} })
                  end,
             },
+            view = {
+               entries = 'native',
+            },
             window = {
                 -- completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
@@ -61,24 +64,24 @@ return {
         require("cmp_git").setup() ]]--
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ '/', '?' }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-              { name = 'buffer' }
-            }
-        })
-
+        -- cmp.setup.cmdline({ '/', '?' }, {
+        --     mapping = cmp.mapping.preset.cmdline(),
+        --     sources = {
+        --       { name = 'buffer' }
+        --     }
+        -- })
+        --
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
-              mapping = cmp.mapping.preset.cmdline(),
-              sources = cmp.config.sources({
-                { name = 'path' }
-            }, {
-                { name = 'cmdline' }
-            }),
-            matching = { disallow_symbol_nonprefix_matching = false }
-        })
-
+        -- cmp.setup.cmdline(':', {
+        --       mapping = cmp.mapping.preset.cmdline(),
+        --       sources = cmp.config.sources({
+        --         { name = 'path' }
+        --     }, {
+        --         { name = 'cmdline' }
+        --     }),
+        --     matching = { disallow_symbol_nonprefix_matching = false }
+        -- })
+        --
         -- Set up lspconfig.
         -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
         -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
