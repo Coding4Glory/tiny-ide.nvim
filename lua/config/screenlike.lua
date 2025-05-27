18c4db3304f100d4f86c6187c662520946ef5f7b
Markus Hergenröder <markus@coding4glory.net>
2025-05-28 00:47:43 +0200

    aligned closer to screen bindings

--- lua/config/screenlike.lua
+++ lua/config/screenlike.lua
@@ -1,35 +1,14 @@
-local function get_editor_count()
-    local all_bufs = vim.api.nvim_list_bufs()
-    local loaded_count = 0
-    for _, b in ipairs(all_bufs) do
-        if vim.fn.buflisted(b) == 1 then
-            loaded_count = loaded_count + 1
-        end
-    end
-    return loaded_count
-end
-
-local function buffer_close(opts)
-    if vim.bo.modified and not opts.bang then
-        vim.api.nvim_err_writeln('Modified buffer save changes (:w) or override with :Bc!')
-        return
-    end
-    if get_editor_count() > 1 then
-        vim.cmd('bn|bd#')
-        return
-    end
-    vim.api.nvim_err_writeln('No buffer left for window. Use :q')
-end
 if not vim.startswith(vim.env["TERM"], "screen") then
     vim.keymap.set('n', '<C-a>t', ":$tabnew<CR>", { desc = 'create [t]ab' })
-    vim.keymap.set('n', '<C-a><Tab>', ":-tabnext<CR>", { desc = 'next <TAB>' })
-    vim.keymap.set('n', '<C-a><S-Tab>', ":+tabnext<CR>", { desc = 'previou<S-TAB>' })
+    -- vim.keymap.set('n', '<C-a><Tab>', ":-tabnext<CR>", { desc = 'next <TAB>' })
+    -- vim.keymap.set('n', '<C-a><S-Tab>', ":+tabnext<CR>", { desc = 'previou<S-TAB>' })
+    vim.keymap.set('n', '<C-a><Tab>', "<C-W> w", { desc = 'next <TAB>' })
+    vim.keymap.set('n', '<C-a><S-Tab>', "<C-W> W", { desc = 'previou<S-TAB>' })
     vim.keymap.set('n', '<C-a>c', ":enew<CR>", { desc = '[n]ew buffer' })
     vim.keymap.set('n', '<C-a>k', ":enew<CR>", { desc = '[k]ill buffer' })
     vim.keymap.set('n', '<C-a>n', ":bn<CR>", { desc = '[n]ext buffer' })
     vim.keymap.set('n', '<C-a>S', ":split<CR>", { desc = '[S]plit buffer horizontally' })
     vim.keymap.set('n', '<C-a>|', ":vsplit<CR>", { desc = 'split buffer vertically' })
-    vim.keymap.set('n', '<C-a>X', buffer_close, { desc = 'close buffer' })
+    -- vim.keymap.set('n', '<C-a>X', buffer_close, { desc = 'close buffer' })
 end
-vim.api.nvim_create_user_command('Bc', buffer_close, {})
-vim.api.nvim_create_user_command('Term', 'split | term', {})
+
