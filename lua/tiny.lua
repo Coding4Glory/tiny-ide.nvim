d77b292a3fa6f117bdafd0920f6a83abc96c416d
Markus Hergenröder <markus@coding4glory.net>
2025-06-23 01:10:14 +0200

    adding template support

--- lua/tiny.lua
+++ lua/tiny.lua
@@ -18,5 +18,6 @@ vim.keymap.set({ 'n', 'i', 'v', 't' }, '<A-Up>', '<C-\\><C-n><C-w>k', { desc = '
 require('tiny.buffers')
 require('tiny.terminal')
 require('tiny.projectsettings')
+require('tiny.templates')
 
 --  vim: set ts=4 sw=4 tw=0 et:
