4046642ff3680adf22b9d57de7d9fafa69bcd5a0
Markus Hergenröder <markus@coding4glory.net>
2025-09-11 20:41:46 +0200

    updated mason slugs

--- lua/plugins/ide/lsp-plugins.lua
+++ lua/plugins/ide/lsp-plugins.lua
@@ -1,10 +1,10 @@
 return {
     {
-        "williamboman/mason.nvim",
+        "mason-org/mason.nvim",
         opts = {},
     },
     {
-        "williamboman/mason-lspconfig.nvim",
+        "mason-org/mason-lspconfig.nvim",
         opts = {
             ensure_installed = {
                 "lua_ls",
