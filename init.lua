2a6e7cf7e4781a1227a610f6c5feb1d391ab42a1
Markus Hergenröder <markus@coding4glory.net>
2025-10-10 18:02:30 +0200

    load builtin before lazy

--- init.lua
+++ init.lua
@@ -5,7 +5,9 @@ require("options")
 -- the initial setup relies on filetype
 vim.cmd("filetype plugin indent on")
 
+-- load builtin micro plugins
+require("builtin").setup({}) -- builtin plugins
+
 -- load plugins
 require("plugins.lazy") -- bootstrap and configure lazy
-require("builtin").setup({}) -- builtin plugins
 
